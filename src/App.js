0f6f8ab43a8f80b22e9ae70946b58d56d4fa3721
abilash <abilashb2000@gmail.com>
2025-02-08 09:22:07 +0530

    image updated

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import "./App.css";
 import Home from "./Pages/Home";
-import "./main.css";
+// import "./main.css";
+import "./styles/main.scss";
+
 //!install "tabler-icons react" first then run the APP
 //!install by this command npm i @tabler/icons-react
 function App() {
